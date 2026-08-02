070613601245ebbc435e0d8160ed562cd23cc7e5
MUDEM-NAGARAJU <nagarajumudem5@gmail>
2026-08-02 20:30:33 +0530

    Add the trip model to admin

--- trips/admin.py
+++ trips/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Trip
+
+
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    list_display = ("id", "bus", "route", "travel_date", "departure_time", "status")
+    list_filter = ("status", "travel_date")
+    search_fields = ("bus__bus_name", "route__source", "route__destination")
